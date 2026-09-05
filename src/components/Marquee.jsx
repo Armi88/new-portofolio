0e9af581c9a6ec04174138e1a7a75855a94b9ad7
unknown <varianarmi78@gmail.com>
2026-09-05 10:46:07 +0700

    update

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -72,7 +72,7 @@ export default function Marquee() {
                 View Work →
               </a>
               <a
-                href="https://drive.google.com/file/d/1XzVRLbW0lNZSRXrkxQ4vVt1y7t1AdHvz/view?usp=sharing"
+                href="https://drive.google.com/file/d/1lnmfub_-Eg6C7Xl7FowQrIONa4D27cCu/view?usp=sharing"
                 className="bio-btn-outline"
                 target="_blank"
                 rel="noopener noreferrer"
